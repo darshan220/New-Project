1847725d1087c7a3194d527bdf09bebb9c85fb94
darshan220 <danpatel22@gmail.com>
2020-07-07 10:40:51 +0530

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,41 @@
+const URL="https://covid19.mathdro.id/api";
+
+let app= angular.module("MyApp",[])
+
+app.controller("MyCtrl",($scope,$http) => {
+        //this is conntroller
+        $scope.title="Stay Home Stay Safe";
+
+        console.log("App Loaded");
+             //calling api
+     $http.get(URL).then(
+         (response) => 
+         {
+             console.log(response.data);
+             $scope.all_data=response.data;
+         },
+        (error) => 
+        {
+             console.log(error);
+        }
+     );
+
+     // get country data
+
+     $scope.get_c_data = () =>{
+            let country = $scope.c;
+            if (country == '')
+            {
+                $scope.c_data=undefined;
+            }
+
+            $http.get(`${URL}/countries/${country}`)
+            .then((response)=>{
+                console.log(response.data);
+                $scope.c_data=response.data;
+                
+            },(error)=>{
+                console.log(error);
+            })
+     };
+});
